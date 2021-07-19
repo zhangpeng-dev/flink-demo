8b712ffc433fbd967d78b96417f8938d678aed3e
ZP <layer5770@163.com>
2021-07-19 21:06:24 +0800

    2

--- src/main/java/com/atguigu/day06/Flink08_Checkpoint.java
+++ src/main/java/com/atguigu/day06/Flink08_Checkpoint.java
@@ -1,5 +1,13 @@
 package com.atguigu.day06;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 /**
  * @ClassName Flink08_Checkpoint
  * @Description TODO
@@ -8,7 +16,29 @@ package com.atguigu.day06;
  * @Version 1.0
  **/
 public class Flink08_Checkpoint {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        System.setProperty("HADOOP_USER_NAME", "atguigu");
+
+        env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/flink/ck"));
+
+        env.enableCheckpointing(5000);
+
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+
+        DataStreamSource<String> source = env.socketTextStream("hadoop102", 9999);
+
+        source.flatMap((FlatMapFunction<String, Tuple2<String, Long>>) (line, out) -> {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG))
+                .keyBy(w -> w.f0)
+                .sum(1)
+                .print();
+
+        env.execute();
     }
 }
